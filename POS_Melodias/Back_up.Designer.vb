fb7d378d921a9ef8cce0b0c7880e009d4db10da7
Carfeir <mauiripasian@gmail.com>
2022-11-13 20:14:40 -0300

    backup de la base de datos(verificar)

--- POS_Melodias/Back_up.Designer.vb
+++ POS_Melodias/Back_up.Designer.vb
@@ -23,82 +23,109 @@ Partial Class Back_up
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Back_up))
-        Me.UserName = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.OldPassword = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtRutaGuardar = New System.Windows.Forms.TextBox()
+        Me.btnRutaGuardar = New System.Windows.Forms.Button()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cboBaseDatos = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
         Me.SuspendLayout()
-        '
-        'UserName
-        '
-        Me.UserName.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower
-        Me.UserName.Location = New System.Drawing.Point(137, 23)
-        Me.UserName.Name = "UserName"
-        Me.UserName.Size = New System.Drawing.Size(122, 20)
-        Me.UserName.TabIndex = 18
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Miriam", 9.0!)
-        Me.Label4.Location = New System.Drawing.Point(18, 61)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(100, 12)
-        Me.Label4.TabIndex = 26
-        Me.Label4.Text = "Nombre de Backup:"
-        '
-        'Button1
-        '
-        Me.Button1.Font = New System.Drawing.Font("Miriam", 9.0!, System.Drawing.FontStyle.Bold)
-        Me.Button1.Location = New System.Drawing.Point(117, 94)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(142, 32)
-        Me.Button1.TabIndex = 24
-        Me.Button1.Text = "&Backup"
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'OldPassword
-        '
-        Me.OldPassword.Location = New System.Drawing.Point(137, 56)
-        Me.OldPassword.Name = "OldPassword"
-        Me.OldPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.OldPassword.Size = New System.Drawing.Size(122, 20)
-        Me.OldPassword.TabIndex = 20
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Miriam", 9.0!)
-        Me.Label1.Location = New System.Drawing.Point(18, 28)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(52, 12)
-        Me.Label1.TabIndex = 19
-        Me.Label1.Text = "Direccion:"
         '
         'Button2
         '
-        Me.Button2.Font = New System.Drawing.Font("Miriam", 9.0!)
-        Me.Button2.Location = New System.Drawing.Point(20, 100)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!)
+        Me.Button2.Location = New System.Drawing.Point(42, 105)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(79, 26)
+        Me.Button2.Size = New System.Drawing.Size(79, 53)
         Me.Button2.TabIndex = 148
         Me.Button2.Text = "&Volver"
         Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(18, 60)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(103, 20)
+        Me.Label3.TabIndex = 156
+        Me.Label3.Text = "Ruta guardar"
+        '
+        'txtRutaGuardar
+        '
+        Me.txtRutaGuardar.Font = New System.Drawing.Font("Courier New", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRutaGuardar.Location = New System.Drawing.Point(137, 58)
+        Me.txtRutaGuardar.Name = "txtRutaGuardar"
+        Me.txtRutaGuardar.Size = New System.Drawing.Size(453, 26)
+        Me.txtRutaGuardar.TabIndex = 155
+        '
+        'btnRutaGuardar
+        '
+        Me.btnRutaGuardar.Image = CType(resources.GetObject("btnRutaGuardar.Image"), System.Drawing.Image)
+        Me.btnRutaGuardar.Location = New System.Drawing.Point(596, 58)
+        Me.btnRutaGuardar.Name = "btnRutaGuardar"
+        Me.btnRutaGuardar.Size = New System.Drawing.Size(103, 26)
+        Me.btnRutaGuardar.TabIndex = 154
+        Me.btnRutaGuardar.Text = "Ruta"
+        Me.btnRutaGuardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnRutaGuardar.UseVisualStyleBackColor = True
+        '
+        'btnConectar
+        '
+        Me.btnConectar.Location = New System.Drawing.Point(361, 24)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(132, 28)
+        Me.btnConectar.TabIndex = 152
+        Me.btnConectar.Text = "Conectar"
+        Me.btnConectar.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(18, 27)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(112, 20)
+        Me.Label5.TabIndex = 151
+        Me.Label5.Text = "Base de datos"
+        '
+        'cboBaseDatos
+        '
+        Me.cboBaseDatos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cboBaseDatos.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboBaseDatos.FormattingEnabled = True
+        Me.cboBaseDatos.Location = New System.Drawing.Point(136, 24)
+        Me.cboBaseDatos.Name = "cboBaseDatos"
+        Me.cboBaseDatos.Size = New System.Drawing.Size(219, 28)
+        Me.cboBaseDatos.TabIndex = 150
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.Button1.Location = New System.Drawing.Point(596, 112)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(103, 46)
+        Me.Button1.TabIndex = 157
+        Me.Button1.Text = "&Backup"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Back_up
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(281, 140)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.UserName)
-        Me.Controls.Add(Me.Label4)
+        Me.ClientSize = New System.Drawing.Size(720, 211)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.OldPassword)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtRutaGuardar)
+        Me.Controls.Add(Me.btnRutaGuardar)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cboBaseDatos)
+        Me.Controls.Add(Me.Button2)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Back_up"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
@@ -107,11 +134,13 @@ Partial Class Back_up
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents UserName As TextBox
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents OldPassword As TextBox
-    Friend WithEvents Label1 As Label
     Private WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtRutaGuardar As TextBox
+    Friend WithEvents btnRutaGuardar As Button
+    Friend WithEvents btnConectar As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents cboBaseDatos As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
 End Class
--- POS_Melodias/Back_up.vb
+++ POS_Melodias/Back_up.vb
@@ -1,9 +1,61 @@
-﻿Public Class Back_up
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+﻿Imports System.IO
+
+Public Class Back_up
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.Close()
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Me.Close()
+
+    'Falta hacer referencia a la BD'
+    'Busca y conecta con cualquier base de datos que se necesite'
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        Dim conString As String = "server=localhost\SQLEXPRESS; uid=; pwd=; database=master"
+
+        Dim conexion As New SqlClient.SqlConnection
+        conexion.ConnectionString = conString
+
+        conexion.Open()
+
+        Dim cmd As New SqlClient.SqlCommand("SELECT name from sys.databases", conexion)
+        Dim da As New SqlClient.SqlDataAdapter(cmd)
+        Dim ds As New DataSet
+        da.Fill(ds)
+        If ds.Tables(0).Rows.Count > 0 Then
+            Dim fila As DataRow
+            For Each fila In ds.Tables(0).Rows
+                cboBaseDatos.Items.Add(fila(0).ToString)
+            Next
+
+            cboBaseDatos.SelectedIndex = 0
+        End If
+    End Sub
+    'Hay que ver si hacerle .bak o .sql
+    'Se selecciona la carpeta donde guardar y guarda la el nombre de la base de datos con la fecha y hora actual 
+    Private Sub btnRutaGuardar_Click(sender As Object, e As EventArgs) Handles btnRutaGuardar.Click
+
+        SaveFileDialog1.Filter = "SQL Backup files|*.bak"
+        SaveFileDialog1.FileName = cboBaseDatos.Text & "-" & Today.Date.ToString("dd-MM-yyyy") & "-" & TimeOfDay.ToString("h.mm") & ".bak"
+
+        If SaveFileDialog1.ShowDialog = Windows.Forms.DialogResult.OK Then
+            txtRutaGuardar.Text = SaveFileDialog1.FileName
+        End If
+
+    End Sub
+
+    'Verificar si se guarda en el lugar seleccionado o lo guarda en la carpeta que crea
+    Private Sub btnBackup_Click(sender As Object, e As EventArgs)
+        Try
+            'Verifica que exista la carpeta, en caso contrario la crea
+            If Not Directory.Exists("D:\Backup") Then
+                Directory.CreateDirectory("D:\Backup")
+            End If
+
+            Process.Start("cmd", "/k" & "sqlcmd -S localhost\SQLEXPRESS -E -Q ""BACKUP DATABASE [" & cboBaseDatos.Text & "] TO DISK='" & txtRutaGuardar.Text & "'""")
+
+            MessageBox.Show("Backup exitoso!", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show(Err.Description)
+        End Try
     End Sub
 End Class
